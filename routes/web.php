31d5abc7bde568909d81822519670b010d011a63
Arseno25 <utomnestagumara@gmail.com>
2025-04-25 13:58:39 +0700

    ✨ (User.php): implement FilamentUser interface and canAccessPanel method to allow all users to access the Filament admin panel 🚚 (web.php): redirect the root route to the /admin route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ExportController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
 
 // Route untuk export data
